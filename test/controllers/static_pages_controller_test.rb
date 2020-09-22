ca2122445bf2abea8ced11d285b9d6c017a04723
sfsharleux <s.harleux@gmail.com>
2020-09-22 17:00:04 +0200

    migrate

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,5 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select 'title', 'About | Ruby on Rails Tutorial Sample App
+'
   end
 end
