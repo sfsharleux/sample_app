91e7f8ac316cf2a633a05e607b1abc6ba95fad04
sfsharleux <s.harleux@gmail.com>
2020-09-22 16:43:35 +0200

    get about test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
